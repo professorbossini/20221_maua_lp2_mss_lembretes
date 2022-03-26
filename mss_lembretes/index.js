41d1406c96a17eea82a4951abd35227f6a4b6eec
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-26 12:13:00 -0300

    Endpoints GET e POST do mss de lembretes. Script de execucao nodemon para o mss de lembretes

---
+++ mss_lembretes/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+const lembretes = {}
+contador = 0
+//GET
+//exemplo.com.br/lembretes
+app.get('/lembretes', (req, res) => {
+    res.send(lembretes)
+})
+ 
+//POST
+//exemplo.com.br/lembretes
+app.post('/lembretes', (req, res) => {
+    contador++
+    //{texto: "Fazer café"}
+    const { texto } = req.body
+    lembretes[contador] = {contador, texto}
+    res.status(201).send(lembrete[contador])
+})
+
+
+
+app.listen(4000, () => {
+    console.log("Lembretes. Porta 4000.")
+})
+
